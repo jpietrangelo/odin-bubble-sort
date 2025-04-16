a1bccb9d085a8be5c74618eb81b3c0d37000913b
Jim Pietrangelo <jmp@webdetours.com>
2025-04-16 18:59:17 -0500

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,10 +2,11 @@
 # frozen_string_literal: true
 
 # bubble_sort.rb
-#
+
 def bubble_sort(nums)
   swaps = 0
   nums.size.downto(0) { |loop_num|
+    swaps = 0
     nums.each_with_index do |num, i|
       test = []
       if i < nums.size - 1
@@ -18,7 +19,7 @@ def bubble_sort(nums)
         end
       end
     end
-    loop_num = 0 if swaps == 1
+    break if swaps == 0
   }
   nums
 end
@@ -27,3 +28,5 @@ p bubble_sort([5,1,3,2,9,8,1,9,7,2])
 # -> [1, 1, 2, 2, 3, 5, 7, 8, 9, 9]
 p bubble_sort([4,3,78,2,0,2])
 # -> [0, 2, 2, 3, 4, 78]
+p bubble_sort([1,2,3,4,6,5])
+# -> [1, 2, 3, 4, 5, 6]
